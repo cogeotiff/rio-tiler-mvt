5412c2f247a61e20f4423227d9d20e0f41e78a9b
vincentsarago <vincent.sarago@gmail.com>
2021-04-06 09:38:56 -0400

    Bump version: 0.0.1dev1 → 0.0.1dev2

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ ext_modules = cythonize(
 
 setup(
     name="rio-tiler-mvt",
-    version="0.0.1dev1",
+    version="0.0.1dev2",
     description=u"""A rio-tiler plugin to encode tile array to MVT""",
     long_description=long_description,
     long_description_content_type="text/markdown",
